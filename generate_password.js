bfad90261d447dd48dc8e2ee2da57d99dc949ffe
deamo771003 <disillusioned2077@gmail.com>
2023-02-21 22:17:04 +0800

    加入判斷勾選、使用者排除字串

--- generate_password.js
+++ generate_password.js
@@ -5,6 +5,7 @@ function generatePassword() {
   const numbers = '1234567890'
   const symbols = '~!@#$%^&*()_+{}:"<>?-=[];,./'
 
+  // define dummy data
   const options = {
     length: '12',
     lowercase: 'on',
@@ -12,11 +13,33 @@ function generatePassword() {
     numbers: 'on',
     excludeCharacters: '04'
   }
-  console.log('options', options)
+
   // create a collection to store things user picked up
+  let collection = []
+  if (options.lowercase === 'on') {
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
+
+  if (options.uppercase === 'on') {
+    collection = collection.concat(upperCaseLetters.split(''))
+  }
+
+  if (options.numbers === 'on') {
+    collection = collection.concat(numbers.split(''))
+  }
+
+  if (options.symbols === 'on') {
+    collection = collection.concat(symbols.split(''))
+  }
 
   // remove things user do not need
-
+  if (options.excludeCharacters) {
+    // collection若包含excludeCharacters內的字串filter就不迭代放入collection
+    collection = collection.filter(
+      character => !options.excludeCharacters.includes(character)
+    )
+  }
+  console.log(collection)
   // start generate password
 
   //return password
